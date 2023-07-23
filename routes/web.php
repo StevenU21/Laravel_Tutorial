6d4a51fef13aa4a7d2e9ab2937e9f8854c7e3ccf
StevenU21 <ulloadeifheltsteven@gmail.com>
2023-07-23 14:49:46 -0600

    Feat: Creación de la Ruta Store

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,4 @@ Route::get('/', function () {
 //Rutas para los Productos
 Route::get('/products', [ProductController::class, 'index'])->name('products.index'); // Ruta para Mostrar los Productos
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); // Ruta para Crear los Productos
+Route::post('/products', [ProductController::class, 'store'])->name('products.store'); // Ruta para Guardar los Productos
