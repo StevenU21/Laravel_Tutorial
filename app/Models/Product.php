ee2215ed245666cda2e05a5b629585207fbedd71
StevenU21 <ulloadeifheltsteven@gmail.com>
2023-07-23 14:08:10 -0600

    refactor: atributos para el modelo de Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'description']; // Campos que se pueden Llenar
+
+    /**
+     * en laravel el modelo para una tabla se crea con el siguiente comando
+     * php artisan make:model Product
+     * este comando creara un archivo en la carpeta app/Models/ con el nombre del modelo
+     * en este caso el archivo se llama Product.php
+     * en este archivo se crea el modelo con los atributos que se desean
+     * el modelo sirve para hacer consultas a la base de datos
+     * y para hacer relaciones entre tablas
+     * tambien para hacer validaciones de los datos que se van a guardar en la base de datos
+     * estos son algunos ejemplos básicos del uso de los modelos
+     */
 }
