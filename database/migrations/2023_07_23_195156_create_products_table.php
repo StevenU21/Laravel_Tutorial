4998e78282a106968cdbe47d89a72a27a96d1e34
StevenU21 <ulloadeifheltsteven@gmail.com>
2023-07-23 14:02:04 -0600

    refactor: añadiendo atributos a la tabla

--- database/migrations/2023_07_23_195156_create_products_table.php
+++ database/migrations/2023_07_23_195156_create_products_table.php
@@ -9,18 +9,31 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up(): void // este metodo es para crear la tabla
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id'); // id
+            $table->string('name', 60); // atributo name con un limite de 60 caracteres
+            $table->text('description'); // detalles o descripción del producto
+            $table->timestamps(); // fecha de creación y actualización
         });
     }
+
+    /**en laravel para crear una tabla o migración debes de ejecutar el siguiente comando de la terminal
+     * php artisan make:migration create_products_table
+     * este comando creara un archivo en la carpeta database/migrations/ con el nombre de la migración y la fecha de creación
+     * en este caso el archivo se llama 2023_07_23_195156_create_products_table.php
+     * en este archivo se crea la tabla con los atributos que se desean
+     * para migrar esta tabla a la base de datos debes de ejecutar el siguiente comando
+     * php artisan migrate
+     * este comando ejecutara todas las migraciones que se encuentren en la carpeta database/migrations/
+     * recuerda que antes debiste configurar el archivo .env con los datos de tu base de datos
+     * */
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down(): void // este metodo es para eliminar la tabla
     {
         Schema::dropIfExists('products');
     }
