2b09e7ccd105abed66e5e3201810abbb7be2e7c6
StevenU21 <ulloadeifheltsteven@gmail.com>
2023-07-24 15:32:23 -0600

    Fix: Nombre del Modelo (Debe ser en Singular)

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Brands extends Model
+class Brand extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'description']; // Campos que se pueden Llenar
