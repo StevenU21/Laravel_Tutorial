fba46fc818dd35eacbec5a63b5cccb17d607ef2e
StevenU21 <ulloadeifheltsteven@gmail.com>
2023-07-24 17:11:34 -0600

    Feat: Creando Seeder para la Tabla Brands

---
+++ database/seeders/BrandsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BrandsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /**
+         * Los seeders son archivos que permiten crear datos de prueba en la base de datos
+         * para crear un seeder se usa el siguiente comando
+         * php artisan make:seeder BrandsTableSeeder
+         * este comando creara un archivo en la carpeta database/seeders/ con el nombre BrandsTableSeeder.php
+         */
+    }
+}
