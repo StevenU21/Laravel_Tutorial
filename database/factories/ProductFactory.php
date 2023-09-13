9783ba9dd553d314be9786216e6fc92315c336ac
everth <everthleonardosaran@gmail.com>
2023-09-13 15:44:51 -0600

    creaciòn de seeder y factory para los productos

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->randomFloat(3, 1, 1000),
+        ];
+    }
+}
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\Product::factory(10)->create();
+        //php artisan db:seed --class=ProductSeeder
+    }
+}
