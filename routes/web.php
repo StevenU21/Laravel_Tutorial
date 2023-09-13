22dd70141dfdb0ba720feb766034b41e21466e61
everth <everthleonardosaran@gmail.com>
2023-09-13 15:34:12 -0600

    creaciòn del login con scafolding

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check()){
+        return redirect()->route('home');
+    }
+    else{
+        return view('auth.login');
+    }
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
